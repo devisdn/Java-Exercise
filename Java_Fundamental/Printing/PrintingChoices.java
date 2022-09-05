ab8fd2c04739e5ba1a6fbb19bad43ea1cd648161
Devis D Nugraha <atagnan98@gmail.com>
2022-09-06 03:22:16 +0700

    Add new folder Printing

---
+++ Java_Fundamental/Printing/PrintingChoices.java
@@ -0,0 +1,10 @@
+public class PrintingChoices{ 
+	public static void main( String[] args){
+		System.out.print("Word1"); 
+		System.out.print("Word2");
+		System.out.println("Word3"); 
+		System.out.print("Line two");
+		System.out.println(); 
+		System.out.println("Line three");
+	} 
+}
